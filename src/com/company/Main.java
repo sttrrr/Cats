6f02392358696f0070b33b42ea04ad034bb3aecc
sttrrr <79934304+sttrrr@users.noreply.github.com>
2021-03-09 21:38:08 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         Cat Vasya = new Cat();
-        Vasya.setWeight(890);
+        Vasya.setWeight(1500);
         System.out.println( Vasya.getCatWeightAndName());
 
         Vasya.setName("Вася");
         System.out.println( Vasya.getCatWeightAndName());
+        Vasya.catEat();
+        Vasya.catEat();
+        Vasya.catEat();
+        Vasya.catEat();
+        Vasya.catEat();
+        Vasya.catEat();
 
-        System.out.println( Vasya.catEat());
-        System.out.println( Vasya.catEat());
-        System.out.println( Vasya.catEat());
-        System.out.println( Vasya.catEat());
-        System.out.println( Vasya.catEat());
-        System.out.println( Vasya.catEat());
-        System.out.println( Vasya.catEat());
         System.out.println( Vasya.getStatus());
         
 
