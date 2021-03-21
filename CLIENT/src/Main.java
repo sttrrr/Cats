a1effe8fdde257a348c005535bae778eb70c1861
Sttrrr <mekad12@mail.ru>
2021-03-21 18:10:19 +0300

    /

--- CLIENT/src/Main.java
+++ CLIENT/src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-public class Main implements Client{
+public class Main{
     public static void main(String[] args) {
         individualBusinessman Tom = new individualBusinessman();
         Scanner in = new Scanner(System.in);
         System.out.print("Введите сумму: ");
-        int amount= in.nextInt();
+        int amount = in.nextInt();
         System.out.println("ИП");
         Tom.showMoney();
         Tom.inputMoney();
