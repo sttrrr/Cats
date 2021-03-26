7b98d2ee3e57d8e5b6711c9f8b1a3d177903f5d4
sttrrr <79934304+sttrrr@users.noreply.github.com>
2021-03-26 20:41:40 +0300

    Add files via upload

---
+++ Regex/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args){
+    Regex regex = new Regex();
+    regex.regex1();
+    }
+}
