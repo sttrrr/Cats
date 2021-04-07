4a1f41a4731cb3ddf5b5dcb58037420d2ffbac36
sttrrr <79934304+sttrrr@users.noreply.github.com>
2021-04-07 21:43:40 +0300

    Update Regex.java

--- Regex v1.1/src/Regex.java
+++ Regex v1.1/src/Regex.java
@@ -1,22 +1,23 @@
 public class Regex {
-    String s = "a2[b3[c]]def";
-    String regexp =".\\[";
-     public void skobki(){
-         while(s.matches(regexp)) {
-             int skobka1 = s.indexOf("]");
-             int skobka2 = s.lastIndexOf("[");
-             int chislo = Character.getNumericValue(s.charAt(skobka2 - 1));
-             System.out.println(skobka2);
-             String substring = s.substring(skobka2, skobka1 + 1);
-             String substringRep = substring.repeat(chislo);
-             substringRep = substringRep.replaceAll("\\[", "");
-             substringRep = substringRep.replaceAll("\\]", "");
-             System.out.println(substring);
-             s = s.replaceAll(substring, substringRep);
 
 
-         }
 
-         System.out.println(s);
-}
-}
+    String regexp ="\\[";
+
+    public void skobki(String s){
+
+    for(int i = 0; i < s.length(); i++){
+            int skobka1 = s.indexOf("]");
+            int skobka2 = s.lastIndexOf("[");
+            int chislo = Character.getNumericValue(s.charAt(skobka2 - 1));
+            System.out.println(skobka2);
+            String substring = s.substring(skobka2, skobka1 + 1);
+            String substringRep = substring.repeat(chislo);
+            substringRep = substringRep.replaceAll("\\[", "");
+            substringRep = substringRep.replaceAll("\\]", "");
+            System.out.println(substring);
+            s = s.replaceAll(substring, substringRep);
+
+        System.out.println(s);
+        skobki(s);
+    }
