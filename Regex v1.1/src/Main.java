75d38761a69db0f0614e11887819df1131ab50da
sttrrr <79934304+sttrrr@users.noreply.github.com>
2021-04-07 21:44:54 +0300

    Update Main.java

--- Regex v1.1/src/Main.java
+++ Regex v1.1/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
          Regex regex = new Regex();
-         regex.skobki();
+         String s = "a2[b3[c]]gef";
+
+         System.out.println(s);
+         regex.skobki(s);
     }
 }
