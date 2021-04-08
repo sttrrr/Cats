63681675ab1695a3792929619fe8c367f025fbe5
sttrrr <79934304+sttrrr@users.noreply.github.com>
2021-04-08 18:40:14 +0300

    Update Regex.java

--- Regex v1.1/src/Regex.java
+++ Regex v1.1/src/Regex.java
@@ -12,12 +12,23 @@ public class Regex {
             int chislo = Character.getNumericValue(s.charAt(skobka2 - 1));
             System.out.println(skobka2);
             String substring = s.substring(skobka2, skobka1 + 1);
+            System.out.println(substring);
             String substringRep = substring.repeat(chislo);
+            String substringS = s.substring(skobka2 - 1,skobka1+1 );
+            System.out.println(substringRep);
             substringRep = substringRep.replaceAll("\\[", "");
             substringRep = substringRep.replaceAll("\\]", "");
             System.out.println(substring);
-            s = s.replaceAll(substring, substringRep);
+            s = s.replace(substringS, substringRep);
+
 
         System.out.println(s);
-        skobki(s);
+
     }
+        skobki(s);
+
+
+}
+
+
+}
