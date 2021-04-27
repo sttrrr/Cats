f3ba33f5d58a180cdcf20e47df4092b764b113d1
Sttrrr <mekad12@mail.ru>
2021-04-27 22:02:28 +0300

    .

--- Search/src/Main.java
+++ Search/src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args){
+        Search c = new Search();
+
+        c.treem();
+
+    }
 }
--- Search/src/Search.java
+++ Search/src/Search.java
@@ -1,2 +1,43 @@
+import java.util.*;
 public class Search {
+
+    TreeMap<Integer,String> person = new TreeMap();
+
+
+
+    String str;
+
+
+    public void treem(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите id или имя");
+
+        boolean intg = sc.hasNextInt();
+        boolean strg = sc.hasNextLine();
+        person.put(0, "Mark");
+        person.put(1, "Jack");
+        person.put(2, "Sofy");
+        person.put(3, "Josh");
+        person.put(4, "Zen");
+        person.put(5, "Pablo");
+        person.put(6, "Dan");
+        person.put(7, "Mary");
+        person.put(8, "Carla");
+        person.put(9, "Stas");
+        if(strg == true){
+            str = sc.nextLine();
+            for (Map.Entry<Integer, String> entry : person.entrySet()) {
+                if (entry.getValue().equals(str)) {
+                    System.out.println(entry.getKey());
+                }
+            }}
+
+        else if(intg == true) {
+            int i = sc.nextInt();
+            Object key;
+            key = person.keySet().toArray()[i];
+            System.out.println(person.get(key));
+        }
 }
+}
+
