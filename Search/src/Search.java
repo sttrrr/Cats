4658d636a91ad2613ae5153a46e6986af0e5ef59
Sttrrr <mekad12@mail.ru>
2021-05-04 12:10:45 +0300

    .

--- Search/src/Search.java
+++ Search/src/Search.java
@@ -14,15 +14,15 @@ public class Search {
 
         boolean intg = sc.hasNextInt();
 
-        person.put(0, "Mark");
-        person.put(1, "Jack");
+        person.put(1559, "Mark");
+        person.put(7756, "Jack");
         person.put(2, "Sofy");
         person.put(3, "Josh");
-        person.put(4, "Zen");
+        person.put(8954, "Zen");
         person.put(5, "Pablo");
-        person.put(6, "Dan");
+        person.put(345, "Dan");
         person.put(7, "Mary");
-        person.put(8, "Carla");
+        person.put(90000, "Carla");
         person.put(9, "Stas");
         person.put(10, "Mattew");
 
@@ -36,9 +36,8 @@ public class Search {
 
         else if(intg == true) {
             int i = sc.nextInt();
-            Object key;
-            key = person.keySet().toArray()[i];
-            System.out.println(person.get(key));
+            String element1 =(String) person.get(i);
+            System.out.println(element1);
         }
 }
 }
