40d00b1df4d58bb80118b6c100c731ac99a4b156
Sttrrr <mekad12@mail.ru>
2021-05-09 17:34:24 +0300

    /

--- Sort/src/Main.java
+++ Sort/src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args){
+        Sort s = new Sort();
+        s.emp();
+    }
+
+
+
 }
--- Sort/src/Sort.java
+++ Sort/src/Sort.java
@@ -1,2 +1,25 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Sort {
+    Map<Integer, String> employee = new HashMap<Integer, String>();
+
+    public void emp() {
+    employee.put(17000, "Иванов");
+    employee.put(51000, "Смирнов");
+    employee.put(26000, "Сидоров");
+    employee.put(26000, "Артемьев");
+    employee.put(43000, "Круглов");
+    employee.put(82000, "Колесников");
+    employee.put(37000, "Перов");
+    employee.put(37000, "Белый");
+    employee.put(45000, "Фирсов");
+
+       // employee.entrySet().stream();
+                //.sorted(Map.Entry.<Integer, String>comparingByValue());
+        List<String> result = employee.entrySet().stream().sorted(
+                Comparator.comparing(n->n.toString())).collect(Collectors.toList());
+    System.out.println(employee);
+
+}
 }
