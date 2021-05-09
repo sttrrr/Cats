3046ada1c60b1251e825822d0fdb131dace46b12
Sttrrr <mekad12@mail.ru>
2021-05-09 13:48:03 +0300

    .

--- Search/src/Search.java
+++ Search/src/Search.java
@@ -28,16 +28,27 @@ public class Search {
 
         if(intg == false){
             str = sc.nextLine();
+            if (person.containsValue(str)){
+
             for (Map.Entry<Integer, String> entry : person.entrySet()) {
                 if (entry.getValue().equals(str)) {
                     System.out.println(entry.getKey());
-                }
-            }}
+                }}
+            }
+            else {
+                System.out.println("Такого значения не существует");
+            }
+        }
 
         else if(intg == true) {
             int i = sc.nextInt();
+            if(person.containsKey(i)){
+
             String element1 =(String) person.get(i);
             System.out.println(element1);
+        }
+        else{
+        System.out.println("Такого id не существует");}
         }
 }
 }
